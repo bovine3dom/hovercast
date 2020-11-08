1294803080ce4108ca3043754cdfc93acb7c7f94
Oliver Blanthorn <freedom4cows@gmail.com>
2020-11-08 20:56:16 +0000

    Fix resumption, make progress

--- src/hovertwit.jl
+++ src/hovertwit.jl
@@ -12,14 +12,22 @@ twitterauth(ENV['API_KEY'],
 # TODO: get tweets around a certain date / time
 # sometimes this doesn't seem to work and just hangs for ages
 # just run it interactively instead
-tweets = get_user_timeline(screen_name = "hovertravelltd", count = 1000)
+#
+# tweets = get_user_timeline(screen_name = "hovertravelltd", count = 1000)
+#
+# hmm, looks like 10k tweets maxes out at 3200 tweets - Hovertravel has 11.6k apparently. Maybe we're limited to more recent ones?
+#
 
 const twitter_time_format = Dates.DateFormat("e u dd HH:MM:SS zzzzz yyyy")
 time2time(t) = ZonedDateTime(t,twitter_time_format)
+import JLD2
+# JLD2.@save "hovercraft.jld2" tidy
+JLD2.@load "hovercraft.jld2" tidy
 
-tidy = DataFrame(time = getfield.(tweets, :created_at) .|> time2time, tweet = getfield.(tweets,:text))
+# tidy = DataFrame(time = getfield.(tweets, :created_at) .|> time2time, tweet = getfield.(tweets,:text))
 
-tidy[contains.(tidy[:tweet],r"services"i),:]
+
+# tidy[contains.(tidy[:tweet],r"services"i),:]
 
 # Sketch:
 # - Hovercraft is assumed to be running
@@ -35,7 +43,7 @@ function resumetweet2time(row)
     ZonedDateTime(date..., time..., tz)
 end
 
-cancellations = []
+cancellations = DataFrame(cancelled = [], resumed = [])
 cancelled = false
 canceltime = ZonedDateTime(0,TimeZone("UTC"))
 resumetime = ZonedDateTime(0,TimeZone("UTC"))
@@ -43,7 +51,7 @@ for tweet in reverse(eachrow(tidy))
     if cancelled && (TimeZones.yearmonthday(tweet.time) != TimeZones.yearmonthday(canceltime))
         date = TimeZones.yearmonthday(canceltime)
         tz = TimeZones.timezone(canceltime)
-        ZonedDateTime(date..., 23, 59, tz)
+        resumetime = ZonedDateTime(date..., 23, 59, tz)
         cancelled = false
         push!(cancellations, (canceltime, resumetime))
     end
@@ -57,4 +65,49 @@ for tweet in reverse(eachrow(tidy))
         push!(cancellations, (canceltime, resumetime))
     end
 end
-cancellations
+cancellations.date = TimeZones.yearmonth.(cancellations.cancelled)
+
+# Most / least unreliable months
+g = groupby(cancellations,:date)
+[(g[i].date[1], size(g[i])) for i in 1:length(g)] # for some reason default iteration over g is weird
+
+timeseries = []
+times = []
+for row in eachrow(cancellations)
+    push!(times,row.cancelled)
+    push!(times,row.resumed)
+    push!(timeseries,1)
+    push!(timeseries,0)
+end
+
+using Plots
+plot(times .|> DateTime,timeseries; legend=:none,seriestype=:step, fill=(0,1,:lightblue))
+
+# Was the hovercraft running at ZonedDateTime t?
+# Might need to improve performance of this if we want to look it up at
+# 30 minute resolutions for historical data over the course of a year
+#
+# Since the rows are sorted, we could first find the last cancelled before our time
+# and give up if it resumes before our time.
+function hoverrun(t)
+    !any(r -> r.cancelled < t < r.resumed, eachrow(cancellations))
+end
+
+
+# So how do we go about predicting this stuff?
+# I think a random forest is probably the best first thing to try - weatherdata(t) to predict hoverrun(t)
+# but it might be wise to also look at weatherdata(t+-/(some time)) to predict hoverrun(t) - i.e. with a sliding window
+
+dates = ZonedDateTime(2019,07,TimeZone("UTC")):Dates.Hour(1):ZonedDateTime(2020,12,TimeZone("UTC"))
+steps = dates |> length
+dummy = DataFrame(dates = dates, winddir = rand((:N,:NE,:E,:SE,:S,:SW,:W,:NW),steps), windspeed = rand(15:30,steps) .- hoverrun.(dates).*10, waveheight = rand(1:0.1:2.5, steps) .- hoverrun.(dates), hover = hoverrun.(dates))
+
+# Now we have dummy data, we just need to predict
+#
+# This looks pretty straightforward:
+# https://github.com/bensadeghi/DecisionTree.jl
+
+
+# For getting real historic data,
+# https://dev.meteostat.net/ looks like it might be OK, but it probably doesn't include wave height
+# Once I have a proof of concept I may have to send a friendly email to the magicseaweed people
